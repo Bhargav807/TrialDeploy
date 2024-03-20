accce9ae577333c35ffeec04e806a84528f97e1d
Bhargav <bhargavkommineni03@gmail.com>
2024-03-20 10:12:21 +0530

    ReadyToDeploy

---
+++ server.js
@@ -0,0 +1,71 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
+import cors from "cors";
+import CategoryRoutes from "./routes/CategoryRoutes.js"
+import CommodityRoutes from "./routes/CommodityRoutes.js"
+import UserRoutes from "./routes/UserRoutes.js"
+import EquipmentRoutes from "./routes/EquipmentRoutes.js"
+import RequirementRoutes from "./routes/RequirementRoutes.js"
+import path from 'path';
+import { fileURLToPath } from 'url';
+//configure environment
+dotenv.config();
+
+//database config
+connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+
+//routes all
+app.use("/api/v1/auth", authRoutes);
+
+
+//category routes
+app.use("/api/v1/category", CategoryRoutes)
+
+
+//for products
+app.use("/api/v1/products", CommodityRoutes)
+
+
+//for equipment
+app.use("/api/v1/equipment",EquipmentRoutes)
+
+//for requirements posting and chat conversational
+app.use("/api/v1/requirements",RequirementRoutes)
+
+//to get userdata
+app.use("/api/v1/users", UserRoutes)
+
+
+//rest api
+app.use("*", function(req, res) {
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
+});
+
+
+
+//const PORT = process.env.PORT;
+
+app.listen(process.env.PORT, () => {
+  console.log(`server running on ${PORT} `.bgCyan.white);
+});
+
+
+
